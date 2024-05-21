62892a3f806bd691e740ce8f3211410dea38b2c2
hmndzzl <167028120+hmndzzl@users.noreply.github.com>
2024-05-20 20:45:59 -0600

    Agregar Carpeta del proyecto

---
+++ fondo_juego/EsqueletoBase.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class EsqueletoBase here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class EsqueletoBase extends Actor
+{
+    /**
+     * Act - do whatever the EsqueletoBase wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        Greenfoot
+    }
+}
---
+++ fondo_juego/Torre.java
@@ -0,0 +1,22 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Torre here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Torre extends Actor
+{
+    /**
+     * Act - do whatever the Torre wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public Torre(){
+        getImage().scale(60,60);
+    }
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
